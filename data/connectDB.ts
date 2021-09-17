c0f03694f2bb2215e5db0719f42e6ca00457dd80
FilipowiczP <przemyslaw.filipowicz1@gmail.com>
2021-09-18 01:44:24 +0200

    test

--- data/connectDB.ts
+++ data/connectDB.ts
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoose = async () => {
   //    === LOCAL DATABASE ===
-  // const url = `mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
-  //    === DEPLOY DATABASE ===
-  const url = `mongodb+srv://przemek:${process.env.PASSWORD}@cluster0.g2rvo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+  const url = `mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
   await mongoose.connect(url);
 };
